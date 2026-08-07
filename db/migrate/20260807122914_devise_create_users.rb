b33ae98bb10665281de263f19699cc721e5f25bf
shosem <shousei4794@icloud.com>
2026-08-08 01:06:11 +0900

    Add: Userモデルの作成、マイグレーション
    - rails g devise Userでモデル・マイグレーション生成
    - nameをカラムに追加
    - migrate実行済み

---
+++ db/migrate/20260807122914_devise_create_users.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class DeviseCreateUsers < ActiveRecord::Migration[8.1]
+  def change
+    create_table :users do |t|
+      ## Database authenticatable
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+      t.string :name, null: false
+
+      t.timestamps null: false
+    end
+
+    add_index :users, :name,                unique: true
+    add_index :users, :email,                unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
+  end
+end
