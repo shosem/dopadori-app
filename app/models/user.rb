eff7b73a8c9b9470c5fbaf68ce324560dad290f5
shosem <shousei4794@icloud.com>
2026-08-08 13:35:16 +0900

    Add: nameカラムを保存できるようにviewとcontrollerをオーバーライド
    - deviseのviewとcontrollerをアプリにコピーしname用の入力欄、ストロングパラメータを追加
    - バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable
+  validates :name, presence: true, length: { maximum: 10 }
 end
