7c630c68872f33c211bd51f8da06919e2bbfa2ee
shosem <shousei4794@icloud.com>
2026-08-09 17:14:56 +0900

    Fix: enumのデフォルト値の指定方法を修正
    修正前はdefault: :immediateをvalidates内に記載していてバリデーションと認識されてしまったため、
    enumの定義行に追加

--- app/models/alternative_action.rb
+++ app/models/alternative_action.rb
@@ -1,6 +1,6 @@
 class AlternativeAction < ApplicationRecord
   belongs_to :user
-  enum :time_span, { immediate: 0, short: 1, preparation: 2 }
+  enum :time_span, { immediate: 0, short: 1, preparation: 2 }, default: :immediate
   validates :title, presence: true, length: { maximum: 30 }
-  validates :time_span, presence: true, default: :immediate
+  validates :time_span, presence: true
 end
