352eb394d8e30ed2e1f9f9d21d8b7f75841121a3
shosem <shousei4794@icloud.com>
2026-08-11 00:00:06 +0900

    Fix: アソシエーションの修正
    urgeモデルがurgeモデル自身に属すると記述していたため修正
    urgeはalternative_actionsに属し、optionalをtrueに

--- app/models/urge.rb
+++ app/models/urge.rb
@@ -1,6 +1,6 @@
 class Urge < ApplicationRecord
   belongs_to :user
-  belongs_to :urge
+  belongs_to :alternative_action, optional: true
 
   enum :resolved, { pending: 0, calmed: 1, took_action: 2, viewed: 3 }, default: :pending
   enum :trigger, { idle: 0, working: 1 }
