1a4b6e722490e8c9f39f7842c4af058e9e2b7cfa
shosem <shousei4794@icloud.com>
2026-08-12 14:16:21 +0900

    Add: モックのデザインを適用
    衝動ボタン編集画面を、モックのデザインに似せて実装。
    triggerのkeyを引数に、翻訳してくれるメソッドと、文言を追加

--- app/models/urge.rb
+++ app/models/urge.rb
@@ -4,4 +4,8 @@ class Urge < ApplicationRecord
 
   enum :resolved, { pending: 0, calmed: 1, took_action: 2, viewed: 3 }, default: :pending
   enum :trigger, { idle: 0, working: 1 }
+
+  def self.trigger_label(key)
+    I18n.t("activerecord.attributes.urge/trigger.#{key}")
+  end
 end
